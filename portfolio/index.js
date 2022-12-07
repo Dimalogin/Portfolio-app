0c6aea850695d81016c041007db17ac533fe88a3
DimaLoginovskiy <remen.2000@mail.ru>
2022-12-08 00:00:03 +0300

    fix: change path for images and change functionality

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,23 +1,6 @@
+import { ruLang, enLang } from "./scripts/date.js";
+import View from "./scripts/view.js";
 
 (function controller() {
-    window.location.hash = "en";
-  
-    addEventListener("hashchange", handleHash);
-    handleHash();
-  })();
-  
-  function handleHash() {
-    const { name } = getRouterInfo();
-  
-    if (name === 'ru' || name === 'en') {
-      const routerName = name + "Route";
-      Controller[routerName]();
-    }
-  }
-  
-  function getRouterInfo() {
-    const hash = location.hash ? location.hash.slice(1) : "";
-    const [name] = hash.split("/");
-  
-    return { name };
-  }
+  View.render(enLang);
+})();
